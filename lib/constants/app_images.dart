910150a1338da7ad5f98c8aacfa2a2a320293e06
Olaoluwa1995 <olakulehinstephen@gmail.com>
2022-03-26 17:35:16 +0100

    Created images constant

---
+++ lib/constants/app_images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static const profilePic = 'assets/images/profile_pic.png';
+  static const image1 = 'assets/images/image1.jpeg';
+  static const image2 = 'assets/images/image2.jpeg';
+  static const image3 = 'assets/images/image3.jpeg';
+  static const image4 = 'assets/images/image4.jpeg';
+  static const image5 = 'assets/images/image5.jpeg';
+  static const image6 = 'assets/images/image6.jpeg';
+  static const image7 = 'assets/images/image7.jpeg';
+}
